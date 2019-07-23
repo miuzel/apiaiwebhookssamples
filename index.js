b8a9dc368f48358d60b12101543a984a0f38d055
张明轩 <mingxuan.zhang@aseit.cn>
2019-07-24 00:52:24 +0800

    update to v2

--- index.js
+++ index.js
@@ -1,112 +1,52 @@
+
+const functions = require('firebase-functions');
 const http = require('http');
 const https = require('https');
 const express = require('express');
 const bodyParser = require('body-parser');
 const uniqueRandomArray = require('unique-random-array');
 const app = express();
-
-app.use(bodyParser.json());
-
+// Import the appropriate class
 const currencyConvertHost = "https://data.fixer.io/api/latest?access_key=6257cba0a2109738cb067e097b3c6f0b&";
 const jokeapi = 'http://api.laifudao.com/open/xiaohua.json';
 const wikiPediaApiHost = 'https://en.wikipedia.org/w/api.php?'; //https://www.mediawiki.org/wiki/API:Opensearch
+
+const { WebhookClient } = require('dialogflow-fulfillment');
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+    const agent = new WebhookClient({ request, response });
+    console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+    console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+    var wikiSearch = async agent => {
+        var searchTerm = agent.parameters.wikiItem;
+        var output = await callWikiPediaApi(searchTerm);
+        let displayText = `关于 ${searchTerm} 好像没有啥特别值得介绍的，改天再找我吧。`;
+        if (output && output[0]) {
+            displayText = `我翻了翻，看看我找到了啥吧 ${output[1][0]}: ${output[2][0]}`;
+        }
+        agent.add(displayText);
+    }
+    var getJoke = async agent => {
+        var output = await callJoke();
+        const randomJoke = uniqueRandomArray(output);
+        const joke = randomJoke().content;
+        agent.add(joke);
+    }
+  
+    let intentMap = new Map(); // Map functions to Dialogflow intent names
+    intentMap.set('wiki.search', wikiSearch);
+    intentMap.set('joke.query', getJoke);
+    agent.handleRequest(intentMap);
+});
+
+
+app.use(bodyParser.json());
 
 app.get('/dummyget', function (req, res) {
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify({ 'speech': 'dummy speech', 'displayText': 'dummy get works!' }));
 });
 
-
-app.post('/webhook', function (req, res) {
-
-    if (req.body.result.parameters['Bored']) {
-        callJoke()
-            .then((output) => {
-                const randomJoke = uniqueRandomArray(output);
-                const joke = randomJoke().content;
-                let result = toApiAiResponseMessage(joke, joke, toTelgramObject(joke, 'Markdown'));
-                res.setHeader('Content-Type', 'application/json');
-                res.send(JSON.stringify(result));
-            })
-            .catch(errorHandler);
-    }
-    else if (req.body.result.parameters['currency-from']) {
-        var currencyFrom = req.body.result.parameters['currency-from'];
-        var currencyTo;
-        if(!req.body.result.parameters['currency-to']){
-            currencyTo = 'USD';
-        }
-        else {
-            currencyTo = req.body.result.parameters['currency-to'];
-        }
-        var number = 1.0;
-        if (req.body.result.parameters['number']) {
-            number = parseFloat(req.body.result.parameters['number']);
-            if (number <= 0) {
-                number = 1.0;
-            }
-        }
-        callFixerIo(currencyFrom, currencyTo)
-            .then((output) => {
-                let resultText = Array();
-                currencyTo.forEach(function (cur) {
-                    var toNumber = number * parseFloat(output.rates[cur.toUpperCase()]);
-                    toNumber = toNumber.toFixed(3);
-                    resultText.push(`${number} ${output.base} = ${toNumber} ${cur}`);
-                }, this);
-
-                let displayText = resultText.join();
-                let result = toApiAiResponseMessage(displayText, displayText, toTelgramObject(resultText.join('\n'), 'Markdown'));
-                res.setHeader('Content-Type', 'application/json');
-                res.send(JSON.stringify(result));
-            });
-    }
-    else if (req.body.result.parameters['wikiItem']) {
-        var searchTerm = req.body.result.parameters['wikiItem'];
-        callWikiPediaApi(searchTerm)
-            .then((output) => {
-                let displayText = `Nothing Found for: ${searchTerm}`;
-                let result;
-                if (output && output[0]) {
-                    displayText = `我翻了翻，看看我找到了啥吧 ${output[1][0]}: ${output[2][0]}`;
-                    let telegramText = htmlEntities(`我翻了翻，看看我找到了啥吧 *${output[1][0]}*: ${output[2][0]} \n\n 查看更多： [WikiPedia](${output[3][0]})`);
-                    result = toApiAiResponseMessage(displayText, displayText, toTelgramObject(telegramText, 'Markdown'));
-                }
-                res.setHeader('Content-Type', 'application/json');
-                if (result) {
-                    res.send(JSON.stringify(result));
-                }
-                else {
-                    res.send(JSON.stringify(displayText));
-                }
-            });
-    }
-    else {
-        res.setHeader('Content-Type', 'application/json');
-        res.send(JSON.stringify({ 'speech': "No Proper hook found", 'displayText': "No Proper hook found" }));
-    }
-});
-
-function callFixerIo(currencyFrom, currencyTo) {
-    return new Promise((resolve, reject) => {
-        currencyTo = currencyTo.join().toUpperCase();
-        let url = `${currencyConvertHost}base=${currencyFrom}&symbols=${currencyTo}`;
-        http.get(url, (res) => {
-            let body = '';
-            res.on('data', (d) => body += d);
-            res.on('end', () => {
-                let jO = JSON.parse(body);
-                resolve(jO);
-            });
-
-            res.on('error', (error) => {
-                reject(error);
-            });
-        });
-    });
-}
-
-function callJoke() {
+var callJoke = () => {
     return new Promise((resolve, reject) => {
         https.get(jokeapi, (res) => {
             let body = '';
@@ -123,7 +63,7 @@ function callJoke() {
     });
 }
 
-function callWikiPediaApi(searchTerm, format = "json", action = "opensearch", limit = 2, profile = "fuzzy") {
+var callWikiPediaApi = (searchTerm, format = "json", action = "opensearch", limit = 2, profile = "fuzzy") => {
     return new Promise((resolve, reject) => {
         let url = `${wikiPediaApiHost}&format=${format}&action=${action}&limit=${limit}&profile=${profile}&search=${searchTerm}`;
         https.get(url, (res) => {
@@ -140,33 +80,6 @@ function callWikiPediaApi(searchTerm, format = "json", action = "opensearch", li
     });
 }
 
-
-function toTelgramObject(text, parse_mode) {
-    return {
-        text: text,
-        parse_mode: parse_mode
-    }
-}
-
-function toApiAiResponseMessage(speech, displayText, telegramObject = null) {
-    return {
-        speech: speech,
-        displayText: displayText,
-        data: {
-            telegram: telegramObject
-        }
-    }
-}
-
-function htmlEntities(str) {
-    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
-}
-
-function errorHandler(error) {
-    console.log(error);
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(toApiAiResponseMessage(error, error, toTelgramObject(error, 'Markdown'))));
-}
 
 app.listen((process.env.PORT || 5000), function () {
     console.log("Server listening");
